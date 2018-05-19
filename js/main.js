e09997f699dea1e3cb63b777eed0a0901ff1306d
david kartuzinski <david@kaidawei.me>
2018-05-19 23:07:42 +0200

    second version of the comming soon site

--- js/main.js
+++ js/main.js
@@ -9,13 +9,13 @@ jQuery(document).ready(function() {
 	/* Main Navigation Clicks */
 	jQuery('.main-nav ul li a').click(function() {
 		var link = jQuery(this).attr('href').substr(1);
-		
+
 		if ( !jQuery('section.content.show, section#' + link).is(':animated') ) {
 			jQuery('.main-nav ul li a').removeClass('active'); //remove active
 			jQuery('section.content.show').addClass('show').animate({'opacity' : 0}, {queue: false, duration: 1000,
 				complete: function() {
 					jQuery('a[href="#'+link+'"]').addClass('active'); // add active
-					jQuery('section#' + link).addClass('show').animate({'opacity' : 1}, {queue: false, duration: 1000});	
+					jQuery('section#' + link).addClass('show').animate({'opacity' : 1}, {queue: false, duration: 1000});
 				}
 			});
 		}
